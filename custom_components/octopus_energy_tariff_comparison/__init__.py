fa39c36451b2557568c26b61f1621b5927bf3a75
David <david@urban-frame.com>
2025-09-10 09:00:35 +0100

    Create __init__.py

---
+++ custom_components/octopus_energy_tariff_comparison/__init__.py
@@ -0,0 +1,35 @@
+"""The Octopus Energy Tariff Comparison integration."""
+from __future__ import annotations
+
+import asyncio
+
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
+
+from .const import DOMAIN
+from .coordinator import OctopusEnergyCoordinator
+
+PLATFORMS: list[Platform] = [Platform.SENSOR]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Octopus Energy Tariff Comparison from a config entry."""
+    coordinator = OctopusEnergyCoordinator(hass, entry.data)
+    
+    await coordinator.async_config_entry_first_refresh()
+    
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = coordinator
+    
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+    
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
